fc4f7be19c71b3e30d00b309437a0658cee629c7
Benjamin Paillereau <bpaillereau@exoplatform.com>
2012-06-28 10:46:26 +0200

    removing gate in dependency, moving back to client side url

--- qrcode/src/main/java/org/gatein/portal/qrcode/Controller.java
+++ qrcode/src/main/java/org/gatein/portal/qrcode/Controller.java
@@ -1,46 +1,29 @@
 package org.gatein.portal.qrcode;
 
-import org.exoplatform.portal.application.PortalRequestContext;
 import juzu.Path;
 import juzu.View;
 import juzu.template.Template;
 
 import javax.inject.Inject;
 import javax.portlet.PortletPreferences;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /** @author <a href="mailto:benjamin.paillereau@exoplatform.com">Benjamin Paillereau</a> */
 public class Controller
 {
 
-   /** . */
-   @Inject
-   @Path("index.gtmpl")
-   Template indexTemplate;
+  /** . */
+  @Inject
+  @Path("index.gtmpl")
+  Template indexTemplate;
 
+  @Inject
+  PortletPreferences portletPreferences;
 
-   @Inject
-   PortletPreferences portletPreferences;
-
-   @Inject
-   public Controller()
-   {
-   }
-
-   @View
-   public void index() throws IOException
-   {
-      PortalRequestContext portalRequestContext = PortalRequestContext.getCurrentInstance();
-
-      String size = portletPreferences.getValue("size", "128");
-      String url = portalRequestContext.getRequest().getRequestURL() + "?" + portalRequestContext.getRequest().getQueryString();
-       
-      Map<String, Object> parameters = new HashMap<String, Object>();
-      parameters.put("url", url);
-      parameters.put("size", size);
-      indexTemplate.render(parameters);
-   }
+  @View
+  public void index()
+  {
+    String size = portletPreferences.getValue("size", "128");
+    indexTemplate.with().set("size", size).render();
+  }
 
 }
